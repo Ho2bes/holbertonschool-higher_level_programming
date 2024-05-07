f329ab7a903964d6afad337ab7396ed8884e17e7
Ho2bes <8696@holbertonstudents.com>
2024-05-07 14:23:33 +0200

    modification with ord

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-result = ""
-    for char in s:
-        if 'a' <= char <= 'z':
-            result += chr(ord(char) - 32)
+    new_str = ""
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            new_str += chr(ord(char) - 32)
         else:
-            result += char
-    print(result)
+            new_str += char
+
+    print(new_str)
