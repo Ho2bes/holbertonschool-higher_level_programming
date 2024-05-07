723c4931736dbce38bfc1e21d651106f14e5a2d3
Ho2bes <8696@holbertonstudents.com>
2024-05-07 11:22:32 +0200

    modification condition with ascii

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 i = 97
 while i < 123:
-    if chr(i) not in ['q', 'e']:
-    print("{}".format(chr(i)), end='')
+    if i != 113 and i != 101:
+        print("{}".format(chr(i)), end='')
     i += 1
