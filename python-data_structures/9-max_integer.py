06c210726146c0bed39f8c33577a52eb3851618d
Ho2bes <8696@holbertonstudents.com>
2024-05-14 10:32:36 +0200

    correct my list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not liste:
+    if not my_list:
         return None
-    max_element = liste[0]
-    for element in liste[1:]:
+    max_element = my_list[0]
+    for element in my_list[1:]:
         if element > max_element:
             max_element = element
             return max_element
