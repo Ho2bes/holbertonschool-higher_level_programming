02be26ff7f5e390ada42d1bc2dbe88982151d724
Ho2bes <8696@holbertonstudents.com>
2024-05-14 10:55:04 +0200

    add divisible by 2 file

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    copied_list = my_list.copy()
+    for element in copied_list:
+        if element % 2 == 0:
+            return True
+        else:
+            return False
