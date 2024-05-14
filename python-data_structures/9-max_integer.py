94666a6c4a5713f5b71b57ad909126713dea6525
Ho2bes <8696@holbertonstudents.com>
2024-05-14 10:24:54 +0200

    add max integer file

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+     if not liste:
+        return None
+    max_element = liste[0]
+    for element in liste[1:]:
+        if element > max_element:
+            max_element = element
+    return max_element
