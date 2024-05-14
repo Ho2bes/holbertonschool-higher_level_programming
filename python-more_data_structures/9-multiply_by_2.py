8173813f74a11c941a3d79ea52502d3b80e6fc53
Ho2bes <8696@holbertonstudents.com>
2024-05-14 16:39:00 +0200

    correct prototype

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
+def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for key, value in a_dictionary.items():
         new_dictionary[key] = value * 2
