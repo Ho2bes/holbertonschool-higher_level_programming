d883fcf2554d0e2c926c4529d4de7dd7ea048de8
Ho2bes <8696@holbertonstudents.com>
2024-05-15 09:18:45 +0200

    change pass by None

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end=" ")
             element += 1
     except IndexError:
-        pass
+        None
     print()
     return element
