9aff3d993444972318ec8c4a5478486f9a79dd24
Ho2bes <8696@holbertonstudents.com>
2024-05-15 09:17:26 +0200

    add 0 in the loop

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     element = 0
     try:
-        for i in range(x):
+        for i in range(0, x):
             print(my_list[i], end=" ")
             element += 1
     except IndexError:
