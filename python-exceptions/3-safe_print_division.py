6384cb1a16369e26fd7db4e5300c7318d3e96720
Ho2bes <8696@holbertonstudents.com>
2024-05-15 11:39:27 +0200

    change the print, add result and modification of except

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        a / b == c
-    except:
-        None
+        result = a / b
+    except ZeroDivisionError:
+        result = None
     finally:
-        print("{:d}".format(c))
+        print("Inside result:{:d}".format(result))
+    return result
 
