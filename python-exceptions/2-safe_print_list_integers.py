0a2ecfd6a16ec7e6ac7a1768edef242d25003151
Ho2bes <8696@holbertonstudents.com>
2024-05-15 11:20:41 +0200

    delete one try and change pass by continue

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     integer = 0
-    try:
-        for i in range(0, x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                integer += 1
-            except (ValueError, TypeError):
-                pass
-    except IndexError:
-        pass
-    print()
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            integer += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
     return integer
