214501589728426b6e436c958686cbe40524d7b0
Ho2bes <8696@holbertonstudents.com>
2024-05-16 16:56:39 +0200

    add commit and @

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,19 +6,21 @@ class Square:
     """contain a simple class of square"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """Initialize a square with option size"""
         self.__size = size
 
     def area(self):
+        """calculate the area of a square"""
         return self.__size ** 2
 
+    @property
     def size(self):
+        """get a size of a square"""
         return self.__size
 
+    @size.setter
     def size(self, value):
+        """define size of square with check"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
