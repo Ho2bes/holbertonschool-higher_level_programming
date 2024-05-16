98ba4c322195030879dcdc87f5c91e98bb15a5a2
Ho2bes <8696@holbertonstudents.com>
2024-05-16 16:35:46 +0200

    put self._size at the end

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,9 +6,8 @@ class Square:
     """contain a simple class of square"""
 
     def __init__(self, size=0):
-        self.__size = size
-        if size is not int:
-            raise TypeError ("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
+        self.__size = size
