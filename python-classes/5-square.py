bc5eb47fc10600d7893ee709f9180be23590780e
Ho2bes <8696@holbertonstudents.com>
2024-05-16 17:05:13 +0200

    add square file

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""contain class Square."""
+
+
+class Square:
+    """Class of a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square with option size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """get a size of square"""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """Define size of square with check"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._size = value
+
+    def area(self):
+        """Calculate and return the area of square."""
+        return self._size ** 2
+
+    def my_print(self):
+        """print a stdout square with #"""
+        if self._size == 0:
+            print("")
+        else:
+            for _ in range(self._size):
+                print("#" * self._size)
