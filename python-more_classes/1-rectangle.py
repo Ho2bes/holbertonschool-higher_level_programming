4b502aa65d8d65e06ad876cb9d32b1b3709f1913
Ho2bes <8696@holbertonstudents.com>
2024-05-21 09:32:54 +0200

    change the order

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,18 +5,18 @@ class Rectangle:
     """contain a simple class of rectangle"""
 
     def __init__(self, width=0, height=0):
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
-
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
 
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+        
     @property
     def width(self):
         return self._width
