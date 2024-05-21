be34a54367e06a36aba58fad688e623f2c3ace44
Ho2bes <8696@holbertonstudents.com>
2024-05-21 14:15:50 +0200

    add : at the end of the if

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     """check if object is class or instance of"""
-    if isinstance(obj, a_class)
+    if isinstance(obj, a_class):
         return True
     else:
         return False
