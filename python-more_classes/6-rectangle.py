9cc48b119a01b8e3a6610c7c7b59da967f565f13
Ho2bes <8696@holbertonstudents.com>
2024-05-21 10:51:47 +0200

    try to correct decrementation

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -19,6 +19,7 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
