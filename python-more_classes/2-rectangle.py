5fad7598ed5defc46d37211b8ce5fe677d9b6a5d
Ho2bes <8696@holbertonstudents.com>
2024-05-21 09:51:50 +0200

    correction of perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -48,4 +48,4 @@ class Rectangle:
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
-        return 2 * (self.__height * self.__width)
+        return 2 * (self.__height + self.__width)
