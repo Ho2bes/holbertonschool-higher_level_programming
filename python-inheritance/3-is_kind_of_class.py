7c6385bc6a3d9fee4e1c9856a91a36e230a96a9c
Ho2bes <8696@holbertonstudents.com>
2024-05-21 14:14:45 +0200

    add kind of class file

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that check if obj is class or an instance of"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if object is class or instance of"""
+    if isinstance(obj, a_class)
+        return True
+    else:
+        return False
