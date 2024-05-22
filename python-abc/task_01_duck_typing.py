f71045069718a9eaec5882a17caff05aeba46d3e
Ho2bes <8696@holbertonstudents.com>
2024-05-22 13:57:29 +0200

    add 01 duck typing file

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""create abstract class and his subclass"""
+
+import math
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+            return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width + self.height) * 2
+
+def shape_info(shape):
+    area = shape.area()
+    perimeter = shape.perimeter()
+    print("Area:", area)
+    print("Perimeter:", perimeter)
+
