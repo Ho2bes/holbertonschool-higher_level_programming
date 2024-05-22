87cf50152f249028d780db6aec26795935e5ff07
Ho2bes <8696@holbertonstudents.com>
2024-05-22 10:05:26 +0200

    change the construction of the code

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""Divide all elements of a matrix"""
+"""divides all elements of a matrix"""
 
 def matrix_divided(matrix, div):
-
+    """divide a matrix by div"""
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    if not isinstance(matrix, list) or \
-            any(not isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    row_length = len(matrix[0])
-    for row in matrix:
-        if len(row) != row_length:
-            raise TypeError("Each row of the matrix must have the same size")
-
     new_matrix = []
     for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
         new_row = []
         for value in row:
+            m = ("matrix must be a matrix (list of lists) of integers/floats")
             if not isinstance(value, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) \
-                                 of integers/floats")
-
+                raise TypeError(m)
             new_row.append(round(value / div, 2))
         new_matrix.append(new_row)
-
-    return new_matrix
+    return (new_matrix)
