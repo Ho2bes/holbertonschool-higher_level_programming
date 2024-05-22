7e177a119d14c7135c4634b66d4bb2ea548a6cbf
Ho2bes <8696@holbertonstudents.com>
2024-05-22 10:30:56 +0200

    add condition about float

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,11 +2,13 @@
 """print a square with the character '#' """
 
 def print_square(size):
-    """
-    print a square
-    """
     if not isinstance(size, int):
-        raise TypeError("size must be an integer")
+        if isinstance(size, float):
+            raise TypeError("size must be an integer")
+        else:
+            raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
+
     print(("#" * size + "\n") * size, end="")
