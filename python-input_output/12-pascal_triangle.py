e822366b42276a7244f14379bf7156f9476368f8
Ho2bes <8696@holbertonstudents.com>
2024-05-27 15:53:55 +0200

    add comment

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """create a pascal triangle"""
 
+
 def pascal_triangle(n):
+    """ create a pascal triangle """
+    triangle = []
+
     if n <= 0:
         return []
 
-    triangle = [[1]]
-
-    for i in range(1, n):
+    for i in range(n):
         row = [1]
         prev_row = triangle[i - 1]
 
