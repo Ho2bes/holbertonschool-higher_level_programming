874b9f2d000b4b9a7816fcb3bbc35a646e0b98ed
Ho2bes <8696@holbertonstudents.com>
2024-05-27 15:41:52 +0200

    add student file

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student"""
+
+
+class Student:
+    "create a student class"
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        "return a dictionnary"
+        if attrs is None:
+            return self.__dict__
+
+        json_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_dict[attr] = getattr(self, attr)
+
+        return json_dict
+
+    def reload_from_json(self, json):
+        """replace attribute info"""
+        if not json:
+            return
+        self.age = json["age"]
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
