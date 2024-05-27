ada21432e5e616b1f0c71369381fde6a02e9e113
Ho2bes <8696@holbertonstudents.com>
2024-05-27 10:37:23 +0200

    add a line

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a function that writes a string to a text file"""
 
+
 def write_file(filename="", text=""):
     """returns the number of characters written"""
     with open(filename, 'w', encoding="utf-8") as file:
