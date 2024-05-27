86b65b703c9ae01d1126524c7c587095f88bf511
Ho2bes <8696@holbertonstudents.com>
2024-05-27 11:29:36 +0200

    use dump

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""function that writes an Object to a text file"""
+"""Definition of to json function """
 
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """use JSON representation"""
-    my_obj_json = json.dumps(my_obj)
-    with open(filename, 'w') as file:
-        file.write(my_obj_json)
+    """ function that writes an Object to a text file"""
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
