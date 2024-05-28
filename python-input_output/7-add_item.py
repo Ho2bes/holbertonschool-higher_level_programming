fc49f97b849483fe2fa067c1248ac0be427e0158
Ho2bes <8696@holbertonstudents.com>
2024-05-28 08:58:12 +0200

    fix pycodestyle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,16 +3,17 @@
 
 
 import sys
-import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from importlib import import_module
 
+save_to_json_file = import_module('5-save_to_json_file').save_to_json_file
+load_from_json_file = import_module('6-load_from_json_file').load_from_json_file
 
 try:
-       json_list = load_from_json_file('add_item.json')
+    json_list = load_from_json_file('add_item.json')
 except FileNotFoundError:
-        json_list = []
+    json_list = []
 
 for i in range(1, len(sys.argv)):
-      json_list.append(sys.argv[i])
+    json_list.append(sys.argv[i])
+
 save_to_json_file(json_list, "add_item.json")
