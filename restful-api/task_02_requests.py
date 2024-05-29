1997cdfcbfb5c4f128e4e0d2244b77085745bfd8
Ho2bes <8696@holbertonstudents.com>
2024-05-29 09:31:30 +0200

    add task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Consuming and processing data from an API using Python"""
+
+
+import requests
+import csv
+
+def fetch_posts_and_titles():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print("Status Code:", response.status_code)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+
+            for post in posts:
+                writer.writerow({'id': post['id'], 'title': post['title'], 'body': post['body']})
+                print(post['title'])
