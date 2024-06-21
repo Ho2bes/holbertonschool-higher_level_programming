490bff643fdd21ad40c2d3f860f4645b03ba2092
Ho2bes <8696@holbertonstudents.com>
2024-06-21 10:28:42 +0200

    fix pycodestyle

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,5 @@
-
 #!/usr/bin/python3
-"""SQLAlchemy script to find a state by name from the database."""
+"""scrpt prnt State object wth name pass arg from the dtb hbtn_0e_6_usa."""
 
 import sys
 from sqlalchemy import create_engine
@@ -9,13 +8,18 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    state_name = sys.argv[4]
+    state = session.query(State).filter(State.name == state_name).first()
+
     if state:
         print(state.id)
     else:
