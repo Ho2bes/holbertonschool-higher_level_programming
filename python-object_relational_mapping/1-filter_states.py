0dae858369c30e9eb0522f73852dda046c3f21a1
Ho2bes <8696@holbertonstudents.com>
2024-06-21 10:04:34 +0200

    add an import

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,20 +2,20 @@
 """Select and filter states that start with 'N'."""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("""SELECT * FROM states
+                 WHERE BINARY name LIKE 'N%' ORDER BY id;""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
-
