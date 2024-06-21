8479fd2e37df61d1f6ee9b2822f45b90e6bfd23b
Ho2bes <8696@holbertonstudents.com>
2024-06-21 10:23:10 +0200

    fix the import

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,5 @@
-
 #!/usr/bin/python3
-"""SQLAlchemy script to print the first state from the database."""
+"""SQLAlchemy script  print first State object fromdatabase hbtn_0e_6_usa."""
 
 import sys
 from sqlalchemy import create_engine
@@ -9,7 +8,9 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
     Base.metadata.create_all(engine)
