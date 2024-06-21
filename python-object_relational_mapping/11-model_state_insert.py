9bb1b6d0727327e17f3523f1e6c863031cdf1c8d
Ho2bes <8696@holbertonstudents.com>
2024-06-21 10:30:57 +0200

    fix the import

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,15 +1,16 @@
-
 #!/usr/bin/python3
-"""SQLAlchemy script to add a new state to the database."""
+"""script add the State object 'Louisiana' to the database hbtn_0e_6_usa."""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import Base
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
     Base.metadata.create_all(engine)
