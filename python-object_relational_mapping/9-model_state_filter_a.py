40220869dfa6db9c63d9b1b887af8c9a50093287
Ho2bes <8696@holbertonstudents.com>
2024-06-21 10:26:22 +0200

    fix create engine and pycodestyle

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,5 @@
-
 #!/usr/bin/python3
-"""SQLAlchemy script to list all states containing the letter 'a' from the database."""
+"""script list * State objects containing letter a from dtb hbtn_0e_6_usa."""
 
 import sys
 from sqlalchemy import create_engine
@@ -9,12 +8,16 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(
+        State.name.like('%a%')
+    ).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
