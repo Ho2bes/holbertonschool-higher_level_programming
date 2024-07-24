efe3600aee2f3513f0385fd94e73ad94b975d525
Ho2bes <8696@holbertonstudents.com>
2024-07-24 10:06:24 +0200

    add code in task 3

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,12 @@
+var toggleHeader = document.getElementById('toggle_header');
+toggleHeader.addEventListener('click', function() {
+    var header = document.querySelector('header');
+
+    if (header.classList.contains('red')) {
+        header.classList.remove('red');
+        header.classList.add('green');
+    } else {
+        header.classList.remove('green');
+        header.classList.add('red');
+    }
+});
