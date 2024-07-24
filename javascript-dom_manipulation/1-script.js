eda246f53495da45be9c5809a1b9c67746023c2f
Ho2bes <8696@holbertonstudents.com>
2024-07-24 10:04:23 +0200

    add all files

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+var redHeader = document.getElementById('red_header');
+redHeader.addEventListener('click', function() {
+    var header = document.querySelector('header');
+    header.classList.add('red');
+});
